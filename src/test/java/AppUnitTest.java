56ddfce5349bebb5e034f1ce75bcf5d5647eb414
Nataliya Bareeva-Miller & Summer Brochtrup <student@epicodus.com>
2015-08-13 09:42:30 -0700

    Template for Java projects

---
+++ src/test/java/AppUnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppUnitTest {
+
+  // @Test
+  // public void checkWinner_rockBeatsScissors_true() {
+  //   RockPaperScissors testResult = new RockPaperScissors();
+  //   assertEquals("Player One Wins", testResult.isWinner("Rock", "Scissors"));
+  // }
+}
